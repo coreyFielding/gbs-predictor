4f2f353919557be72ccefc3b3567a7070b794b72
Stefanie Calder <stefanie@insitesdigital.com>
2022-03-09 15:57:09 +0000

    feat: adjusted tabs to change size on tablet

--- components/organisms/Sidebar/index.js
+++ components/organisms/Sidebar/index.js
@@ -4,8 +4,17 @@ export const Sidebar = () => {
   return (
       <div className={styles.sidebar}>
       <aside className={styles.sidebar_inner}>
-        <div className={styles.sidebar_tab}></div>
-        <div className={styles.sidebar_tab}></div>
+        <div className={styles.sidebar_tab}>
+          <div>
+            <h2>Genesis International</h2>
+            <p>PGA Tour</p>
+          </div>
+        </div>
+        <div className={styles.sidebar_tab}>
+          <div>
+            <h2>Honda Classic</h2>
+          </div>
+        </div>
 
               <div className={styles.sidebar_socials}></div>
       </aside>
