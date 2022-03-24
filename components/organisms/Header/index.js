c7ec71b861b6806c234f7850823d7a8e8eb1708a
Stefanie Calder <stefanie@insitesdigital.com>
2022-03-24 11:27:37 +0000

    fix: adding links around social icons and back button

--- components/organisms/Header/index.js
+++ components/organisms/Header/index.js
@@ -18,10 +18,12 @@ export const Header = ({ className }) => {
         {/* <Logo /> */}
         <div className={styles.header_inner}>
             <div className="header__left-side">
-            <svg className="back-button" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="15" cy="15" r="14.5" transform="rotate(-180 15 15)" stroke="#254933"/>
-              <path d="M18 9L12 15L18 21" stroke="#254933"/>
-            </svg>
+            <a href="#">
+              <svg className="back-button" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="15" cy="15" r="14.5" transform="rotate(-180 15 15)" stroke="#254933"/>
+                <path d="M18 9L12 15L18 21" stroke="#254933"/>
+              </svg>
+            </a>
 
 
             <svg className="header-logo" width="176" height="40" viewBox="0 0 176 40" fill="none" xmlns="http://www.w3.org/2000/svg">
