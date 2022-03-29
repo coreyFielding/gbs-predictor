b4823d24335950e4f6b2c98f7a6a90f3a1f96169
Donnie Berry <donniejrberry@gmail.com>
2022-03-29 17:11:45 +0100

    fix: Positions now being applied correctly

--- hooks/usePlayers.js
+++ hooks/usePlayers.js
@@ -1,13 +1,13 @@
 // Context
-import { useContext } from "react"
-import { PlayerContext } from "contexts/PlayerProvider"
-import { useVariable } from "./useVariable"
+import { useContext } from "react";
+import { PlayerContext } from "contexts/PlayerProvider";
+import { useVariable } from "./useVariable";
 
 export const usePlayers = () => {
-  const { players: playerList, variables } = useContext(PlayerContext)
-  const { groups } = useVariable()
+  const { players: playerList, variables } = useContext(PlayerContext);
+  const { groups } = useVariable();
 
-  let newPlayerList = []
+  let newPlayerList = [];
 
   // Get the dynamic weights from each variable group
   const weights = groups
@@ -16,71 +16,73 @@ export const usePlayers = () => {
         return {
           variable: item.Child_Name,
           weight: item.childWeight,
-        }
+        };
       })
     )
-    .flat()
+    .flat();
 
   // Assign weights and positions to each player based on variable
 
   const assignWeightsAndPositions = () => {
     variables?.forEach(({ Variable }, variableIndex) => {
       Variable.data.map((item, index) => {
-        playerList[index].weights = weights ? [...weights] : []
+        const player = playerList.find(
+          (player) => player.Player === item.Player
+        );
 
-        console.log(item.Position) // This gives a position of 38 for David
+        const playerIndex = playerList.indexOf(player);
 
-        // Add player position for each variable type
-        playerList[index].weights[variableIndex] = {
-          ...playerList[index].weights[variableIndex],
-          position: Number(item.Position), // <-- Here is where the position becomes incorrect
-        }
+        playerList[playerIndex].weights = weights ? [...weights] : [];
 
-        // Push player with weights and positions to new player list
+        playerList[playerIndex].weights[variableIndex] = {
+          ...playerList[playerIndex].weights[variableIndex],
+          position: parseFloat(item.Position), // <-- Here is where the position becomes incorrect
+        };
+
         newPlayerList.push({
-          ...playerList[index],
-          weights: [playerList[index].weights[variableIndex]],
-        })
-      })
-    })
-  }
+          ...playerList[playerIndex],
+          weights: [playerList[playerIndex].weights[variableIndex]],
+        });
+      });
+    });
+  };
 
   // Assign score to each player based on variable position and weighting
 
   const getPlayerScore = () => {
     newPlayerList?.forEach((player) => {
-      let scoreTotal = 0
+      let scoreTotal = 0;
 
       // Find player across all variables
       const playerFromAllVariables = newPlayerList.filter(
         (item) => item.Player === player.Player
-      )
+      );
 
       playerFromAllVariables.forEach((item) => {
         if (item.weights.length) {
           item.weights?.forEach(({ position, weight }) => {
-            scoreTotal += (51 - position) * weight
-          })
+            scoreTotal += (51 - position) * weight;
+          });
         }
-      })
+      });
 
       // Add score property to each player in original player list
       const foundPlayer = playerList.find(
         (item) => item.Player === player.Player
-      )
+      );
 
-      playerList[playerList.indexOf(foundPlayer)].score = scoreTotal
-    })
-  }
+      playerList[playerList.indexOf(foundPlayer)].score = scoreTotal;
+    });
+  };
 
-  assignWeightsAndPositions()
-  getPlayerScore()
+  assignWeightsAndPositions();
+  getPlayerScore();
 
   const sortedPlayerList = playerList?.sort((a, b) =>
     a.score < b.score ? 1 : -1
-  )
+  );
 
   return {
     sortedPlayerList,
-  }
-}
+  };
+};
